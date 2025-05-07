4bd2ff38fa92d4574c8bb78888106d540cd8d6ff
Chungharon <ngairaharon@gmail.com>
2025-05-07 19:35:57 +0300

    setup

---
+++ multitenant-ecommerce/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl focus:hover:text-rose-600">
+        hello
+      </p>
+    </div>
+  );
+};
