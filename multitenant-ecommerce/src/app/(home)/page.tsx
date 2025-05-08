d730c9bbdb47b9ffe7b68ca032abff7a00d3d402
Chungharon <ngairaharon@gmail.com>
2025-05-08 15:05:26 +0300

    03: Home Layout

--- multitenant-ecommerce/src/app/(home)/page.tsx
+++ multitenant-ecommerce/src/app/(home)/page.tsx
@@ -4,7 +4,9 @@ import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Progress } from "@/components/ui/progress";
 
+
 export default function Home() {
+
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
