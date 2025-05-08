d0da1f190539932e8e95e6a68fe0c1794857ad93
Chungharon <ngairaharon@gmail.com>
2025-05-08 10:12:05 +0300

    02: customization

--- multitenant-ecommerce/src/app/page.tsx
+++ multitenant-ecommerce/src/app/page.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-3xl focus:hover:text-rose-600">
-        hello
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            Click Me
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="input text" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="i am textarea" />
+        </div>
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
-};
+}
