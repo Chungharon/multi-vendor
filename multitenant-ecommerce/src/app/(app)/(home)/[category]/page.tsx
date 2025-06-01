f47a4289a7bb4eaa4de3181d795110192b3e8a9e
Chungharon <ngairaharon@gmail.com>
2025-06-01 15:11:56 +0300

    11: Products

--- multitenant-ecommerce/src/app/(app)/(home)/[category]/page.tsx
+++ multitenant-ecommerce/src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,8 @@
+import { ProductsList, ProductsListSkeleton } from "@/modules/products/ui/components/product-list";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
     params: Promise<{
         category: string;
@@ -6,10 +11,20 @@ interface Props {
 
 const Page = async ({ params }: Props) => {
     const { category } = await params;
+    
+
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category,
+    }))
+
     return (
-        <div>
-            Category:   {category}
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductsListSkeleton />}>
+                <ProductsList category={category} />
+            </Suspense>
+            
+        </HydrationBoundary>
     )
 };
 
--- multitenant-ecommerce/src/trpc/routers/_app.ts
+++ multitenant-ecommerce/src/trpc/routers/_app.ts
@@ -2,8 +2,10 @@ import { createTRPCRouter } from '../init';
 // Make sure that 'authRouter' is exported from the target module, or import the correct member.
 import { authRouter } from '@/modules/auth/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { productsRouter } from '@/modules/products/server/procedures';
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  products: productsRouter,
   Categories: categoriesRouter
 });
 // export type definition of API
